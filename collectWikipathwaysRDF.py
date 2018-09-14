9acfbef6960eb41ce8e5733e5bd220bc44c524ed
andrawaag <andra@waagmeester.net>
2018-09-14 16:33:24 +0200

    Updated the file to work with the generic URL

--- collectWikipathwaysRDF.py
+++ collectWikipathwaysRDF.py
@@ -5,22 +5,43 @@ import io
 import zipfile
 from contextlib import closing
 from rdflib import Graph
+from bs4 import BeautifulSoup, SoupStrainer
 
-u = requests.get("http://data.wikipathways.org/current/rdf/wikipathways-20170410-rdf-gpml.zip")
+#u = requests.get("http://data.wikipathways.org/current/rdf/wikipathways-20171210-rdf-gpml.zip")
 
 temp = Graph()
-with closing(u), zipfile.ZipFile(io.BytesIO(u.content)) as archive:
-    for member in archive.infolist():
-        nt_content = archive.read(member)
-        print(nt_content)
-        temp.parse(data=nt_content.decode(), format="turtle")
 
-u = requests.get("http://data.wikipathways.org/current/rdf/wikipathways-20170410-rdf-wp.zip")
-with closing(u), zipfile.ZipFile(io.BytesIO(u.content)) as archive:
-    for member in archive.infolist():
-        nt_content = archive.read(member)
-        print(nt_content)
-        temp.parse(data=nt_content.decode(), format="turtle")
+url = 'http://data.wikipathways.org/current/rdf'
+page = requests.get(url).text
+files = []
+for link in BeautifulSoup(page, "lxml", parse_only=SoupStrainer('a')):
+    address = str(link).split("\"")
+    if len(address) > 1:
+        filename = address[1].replace("./", "/")
+        if len(filename) > 1:
+            if filename not in files:
+                if filename != "./":
+                    files.append(url + filename)
+    wpids = []
+    for file in set(files):
+        if "rdf-wp" in file:  # get the most accurate file
+            print(file)
+            u = requests.get(file)
+            with closing(u), zipfile.ZipFile(io.BytesIO(u.content)) as archive:
+                for member in archive.infolist():
+                    nt_content = archive.read(member)
+                    # print(nt_content)
+                    temp.parse(data=nt_content.decode(), format="turtle")
 
 temp.serialize("/tmp/wikipathways.ttl", format="turtle")
+
+# u = requests.get("http://data.wikipathways.org/current/rdf/wikipathways-20180910-rdf-wp.zip")
+# with closing(u), zipfile.ZipFile(io.BytesIO(u.content)) as archive:
+#    for member in archive.infolist():
+#        nt_content = archive.read(member)
+#        print(nt_content)
+#        temp.parse(data=nt_content.decode(), format="turtle")
+#        print(len(temp))
+
+
 
